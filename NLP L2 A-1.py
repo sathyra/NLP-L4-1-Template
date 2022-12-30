dbace14c1f76420be6f481c39bbd60a22d0d1464
sathyra <105861021+sathyra@users.noreply.github.com>
2022-12-30 12:11:22 +0530

    Add files via upload

---
+++ NLP L2 A-1.py
@@ -0,0 +1,13 @@
+#open the text file
+text_file=open()
+
+#read the data
+read_text=text_file.read()
+
+#Datatype of the data read
+print(type(read_text))
+print("\n")
+
+#print the entire story text
+
+#length of the text
